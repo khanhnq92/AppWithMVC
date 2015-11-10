52e7bf80ee831c789c6aafb9bafd789d36fb1b26
Huy Quang Ngo <manhle@huys-mac-mini.local>
2015-11-10 15:21:54 +0700

    a

---
+++ ExampleWithMVC/ExampleWithMVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExampleWithMVC
+//
+//  Created by Huy Quang Ngo on 11/10/15.
+//  Copyright © 2015 KhanhNQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
